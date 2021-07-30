68ee4a558b01fbad388a8293be27eb9199a6c1c4
Natking007 <77073455+Natking007@users.noreply.github.com>
2021-07-29 23:07:29 -0400

    New test case2, more changes

--- src/Test2.java
+++ src/Test2.java
@@ -1,5 +1,7 @@
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("Testing log out or something");
+
+        System.out.println("Do one more");
     }
 }
